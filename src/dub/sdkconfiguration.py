ba01e4fdcaf21e25af5237bf565fe051020cb916
regression-test-executor <regression-test-executor@speakeasy.com>
2024-09-24 11:25:29 +0000

    openapi-generation@4cffc640630709c5612ece4d2e6b66cf49224021

--- src/dub/sdkconfiguration.py
+++ src/dub/sdkconfiguration.py
@@ -30,8 +30,8 @@ class SDKConfiguration:
     language: str = "python"
     openapi_doc_version: str = "0.0.1"
     sdk_version: str = "0.10.2"
-    gen_version: str = "2.421.3"
-    user_agent: str = "speakeasy-sdk/python 0.10.2 2.421.3 0.0.1 dub"
+    gen_version: str = "2.422.12"
+    user_agent: str = "speakeasy-sdk/python 0.10.2 2.422.12 0.0.1 dub"
     retry_config: OptionalNullable[RetryConfig] = Field(default_factory=lambda: UNSET)
     timeout_ms: Optional[int] = None
 
