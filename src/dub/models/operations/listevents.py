166496569d22285521bcd81bd8c4a3e9f3dfcf9c
regression-test-executor <regression-test-executor@speakeasy.com>
2024-10-01 15:37:42 +0000

    openapi-generation@a00713d3cbed94ba3d5a1fe865fa0c0220f182f5

--- src/dub/models/operations/listevents.py
+++ src/dub/models/operations/listevents.py
@@ -12,8 +12,8 @@ from dub.types import BaseModel
 from dub.utils import FieldMetadata, QueryParamMetadata
 from enum import Enum
 import pydantic
-from typing import List, Optional, TypedDict, Union
-from typing_extensions import Annotated, NotRequired
+from typing import List, Optional, Union
+from typing_extensions import Annotated, NotRequired, TypedDict
 
 
 class QueryParamEvent(str, Enum):
